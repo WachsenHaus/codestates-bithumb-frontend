6f1d73948f52dabbdcf9f027c7c6fa220650f64f
wachsenhaus <bi9choi@gmail.com>
2022-04-13 02:22:08 +0900

    <추가> : <타임스탬프에 대한 기능 추가>

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,30 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { createChart, CrosshairMode } from 'lightweight-charts';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { createChart, CrosshairMode, UTCTimestamp } from 'lightweight-charts';
 import { useRecoilValue } from 'recoil';
 import { ITickerReceiverTypes, tickerReceiveState } from '../atom/user.atom';
 import produce from 'immer';
 import axios from 'axios';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement
-  // Title,
-  // Tooltip,
-  // Legend
-);
+import moment from 'moment';
 
 const MainContent = () => {
   const labels = ['BTC_KRW'];
@@ -97,9 +77,20 @@ const MainContent = () => {
         if (result.data) {
           const idx = result.data.t.length;
           let isUp;
+          // 타임 간격이 1분이라면.10분/30분/1시간/1일 이런식이라면 시간의 차이를 구한뒤 오늘로부터 역순으로 모멘트를 사용해서 시간을 넣줘야한다.
+          console.log(moment().format('YYYY-MM-DD / HH : mm : ss'));
+          console.log(
+            moment().add('minute', '-1').format('YYYY-MM-DD / HH : mm : ss')
+          );
+          const CONST_KR_UTC = 9 * 60 * 60 * 1000;
+
+          // idx 1500이잔항. 근데 인덱스 0부터 시작한다고. -1500, 2*1500 idx 1500+ i
           for (let i = 0; i < idx; i++) {
+            const time = ((result.data.t[i] + CONST_KR_UTC) /
+              1000) as UTCTimestamp;
             chartObj.push({
-              time: result.data.t[i],
+              time: time,
+              // time: result.data.t[i],
               open: result.data.o[i],
               high: result.data.h[i],
               low: result.data.l[i],
@@ -128,6 +119,11 @@ const MainContent = () => {
 
             timeScale: {
               borderColor: 'rgba(197, 203, 206, 0.8)',
+            },
+          });
+          chart.applyOptions({
+            timeScale: {
+              timeVisible: true,
             },
           });
           const chart2 = createChart(wrapperRef.current, {
