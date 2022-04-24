495d33c77e24e3ec2c1e0ac0cf76aae65c515148
wachsenhaus <bi9choi@gmail.com>
2022-04-25 02:49:24 +0900

    <수정> : <초기화부분 reset으로 테스트함>

--- src/hooks/useResetDrawData.tsx
+++ src/hooks/useResetDrawData.tsx
@@ -9,26 +9,29 @@ import { atomCoinBar, atomDrawCoinBar } from '../atom/coinBar.atom';
 import { atomDrawChart, atomDrawCoinInfo } from '../atom/drawData.atom';
 import { atomOrderBook } from '../atom/orderBook.atom';
 import { atomSelectCoinDefault } from '../atom/selectCoinDefault.atom';
-import { atomFinalTransaction } from '../atom/total.atom';
-
-import { atomChartData } from '../atom/tvChart.atom';
+import {
+  atomDrawTransaction,
+  atomFinalTransaction,
+  atomTransactions,
+} from '../atom/total.atom';
 
 /**
  * 코인이 선택되면 그리는 값들을 초기화하는 기능입니다.
  */
 const useResetObserverDrawData = () => {
   const selectCoin = useRecoilValue(atomSelectCoinDefault);
-  const drawTranSaction = useSetRecoilState(atomFinalTransaction);
+  const drawTransaction = useSetRecoilState(atomDrawTransaction);
+  // const rowTransaction = useSetRecoilState(atomTransactions);
   const drawChartData = useSetRecoilState(atomDrawChart);
-  const resetCoinBar = useResetRecoilState(atomDrawCoinBar);
-  // const resetCoinDetail = useResetRecoilState(atomSelectCoinDetail);
-  // const drawCoinInfo = useSetRecoilState(atomCoinBar);
   const drawOrderBook = useSetRecoilState(atomOrderBook);
 
+  const resetCoinBar = useResetRecoilState(atomDrawCoinBar);
+  const resetRowTransaction = useResetRecoilState(atomTransactions);
+
   useEffect(() => {
-    drawTranSaction([]);
+    drawTransaction([]);
     drawChartData([]);
-    // resetCoinDefault();
+    resetRowTransaction();
     resetCoinBar();
     drawOrderBook({
       ask: [],
