f00f7ddb8140b90a9d2fde314ec85268e6d0ca3f
wachsenhaus <bi9choi@gmail.com>
2022-04-28 22:56:36 +0900

    <수정> : <webworker에서  lodash제거>

--- src/hooks/worker/selectorPriceFilterdCoins.js
+++ src/hooks/worker/selectorPriceFilterdCoins.js
@@ -3,16 +3,44 @@
 //
 
 const workercode = () => {
-  importScripts(
-    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js'
-  );
+  // importScripts(
+  //   'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js'
+  // );
   order = (orderMode, datas, sortDirection) => {
+    // console.log(datas);
     if (orderMode === 'e') {
-      return _.orderBy(datas, [(e) => Number(e.e)], [sortDirection]);
+      return datas.sort((a, b) => {
+        const current = Number(a.e);
+        const next = Number(b.e);
+        if (sortDirection === 'asc') {
+          return current < next ? -1 : current > next ? 1 : 0;
+        } else {
+          return current > next ? -1 : current < next ? 1 : 0;
+        }
+      });
+      // return _.orderBy(datas, [(e) => Number(e.e)], [sortDirection]);
     } else if (orderMode === 'r') {
-      return _.orderBy(datas, [(e) => Number(e.r)], [sortDirection]);
+      // return _.orderBy(datas, [(e) => Number(e.r)], [sortDirection]);
+      return datas.sort((a, b) => {
+        const current = Number(a.r);
+        const next = Number(b.r);
+        if (sortDirection === 'asc') {
+          return current < next ? -1 : current > next ? 1 : 0;
+        } else {
+          return current > next ? -1 : current < next ? 1 : 0;
+        }
+      });
     } else {
-      return _.orderBy(datas, [(e) => Number(e.u24)], [sortDirection]);
+      // return _.orderBy(datas, [(e) => Number(e.u24)], [sortDirection]);
+      return datas.sort((a, b) => {
+        const current = Number(a.u24);
+        const next = Number(b.u24);
+        if (sortDirection === 'asc') {
+          return current < next ? -1 : current > next ? 1 : 0;
+        } else {
+          return current > next ? -1 : current < next ? 1 : 0;
+        }
+      });
     }
   };
 
